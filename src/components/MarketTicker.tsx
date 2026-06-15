05d1c0b1ce63156ff815035024e12d6d43bdd84e
Dorah <momanyidora@gmail.com>
2026-06-15 14:55:39 +0300

    Add live market ticker section

--- src/components/MarketTicker.tsx
+++ src/components/MarketTicker.tsx
@@ -1,13 +1,18 @@
-interface Props {
-  pairs: string[];
+interface MarketItem {
+  pair: string;
+  rate: number;
 }
 
-export default function MarketTicker({ pairs }: Props) {
+interface Props {
+  items: MarketItem[];
+}
+
+export default function MarketTicker({ items }: Props) {
   return (
-    <div className="overflow-x-auto whitespace-nowrap">
-      {pairs.map((pair) => (
-        <span key={pair} className="mr-6">
-          {pair}
+    <div className="bg-slate-900 text-white p-3 overflow-x-auto whitespace-nowrap">
+      {items.map((item) => (
+        <span key={item.pair} className="mr-8">
+          {item.pair}: {item.rate}
         </span>
       ))}
     </div>
